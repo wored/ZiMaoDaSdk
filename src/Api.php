c35225e893c2349f097346cafba29c3725de1bd3
qiliu <qiliuf@gmail.com>
2020-05-12 14:48:43 +0800

    添加创建订单及取消订单

--- src/Api.php
+++ src/Api.php
@@ -8,6 +8,8 @@ use Psr\Http\Message\ResponseInterface;
 
 class Api extends AbstractAPI
 {
+    public $config;
+    public $params;
 
     /**
      * Api constructor.
@@ -18,12 +20,28 @@ class Api extends AbstractAPI
      */
     public function __construct(ZiMaoDaSdk $ziMaoDaSdk)
     {
-        Helper::dd($ziMaoDaSdk->getConfig());
+        $this->config = $ziMaoDaSdk->getConfig();
+        $this->params = [
+            'nick'   => $this->config['nick'],
+            'name'   => $this->config['name'],
+            'format' => $this->config['format'],
+        ];
     }
 
-    public function request($method, $url, $params)
+    /**
+     * @param string $urlPath
+     * @param array $params
+     * @return mixed
+     */
+    public function request(string $urlPath, array $params)
     {
-
+        $http = $this->getHttp();
+        $params = array_merge($this->params, $params);
+        $params['date'] = date('Y-m-d H:i:s', time());
+        $params['sign'] = $this->sign($params);
+        $requestUrl = $this->config['rootUrl'] . '/webAPI/' . $urlPath;
+        $response = call_user_func_array([$http, 'POST'], [$requestUrl, $params]);
+        return json_decode(strval($response->getBody()), true);
     }
 
     /**
@@ -31,8 +49,52 @@ class Api extends AbstractAPI
      * @param array $request_params
      * @return string
      */
-    public function sign(array $request_params)
+    public function sign(array $params)
     {
+        $data = '';
+        $data .= base64_encode($params['nick']);
+        $data .= base64_encode($params['method']);
+        $data .= base64_encode($params['date']);
+        $data .= base64_encode($params['name']);
+        $data .= base64_encode($params['orderNumber']);
+        $data .= base64_encode($params['format']);
+        return md5($data);
+    }
 
+    /**
+     * @param $param
+     * @param bool $root
+     * @return string
+     */
+    public function paramToXml($param, $root = true)
+    {
+        if ($root) {
+            $xml = '<?xml version="1.0" encoding="utf-8"?>';
+        } else {
+            $xml = '';
+        }
+        foreach ($param as $key => $vo) {
+            if ($key === 'attributes') {//判断是否是属性字段
+                continue;
+            }
+            if (!is_numeric($key)) {
+                $xml .= "<{$key}";
+                if (!empty($vo['attributes'])) {//添加属性
+                    foreach ($vo['attributes'] as $item => $attribute) {
+                        $xml .= " {$item}=\"{$attribute}\"";
+                    }
+                }
+                $xml .= '>';
+            }
+            if (is_array($vo) and count($vo) > 0) {
+                $xml .= $this->paramToXml($vo, false);
+            } else {
+                $xml .= $vo;
+            }
+            if (!is_numeric($key)) {
+                $xml .= "</{$key}>";
+            }
+        }
+        return $xml;
     }
 }
--- src/ZiMaoDaSdk.php
+++ src/ZiMaoDaSdk.php
@@ -21,4 +21,37 @@ class ZiMaoDaSdk extends Foundation
         $config['debug'] = $config['debug'] ?? false;
         parent::__construct($config);
     }
+
+    /**
+     * 订单信息创建到自贸达
+     * 客户网站上的订单创建到ZMD报关系统，采用post方式传输，传入的数据为xml文件
+     * @param array $order 以数组的信息拼装
+     * @return mixed
+     */
+    public function createOrder(array $order)
+    {
+        $params = [
+            'method'      => 'swapi.web.order.create',
+            'orderDate'   => $order['OWebOrder']['orderDate'],
+            'orderNumber' => $order['OWebOrder']['orderNumber'],
+            'orderData'   => $this->api->paramToXml($order),
+        ];
+        return $this->api->request('createOrder', $params);
+    }
+
+    /**
+     * 取消订单
+     * 客户订单从自贸达系统取消，采用post方式传输，传入的数据为xml文件
+     * @param array $order
+     * @return mixed
+     */
+    public function refundOrder(array $order)
+    {
+        $params = [
+            'method'      => 'swapi.web.order.refund',
+            'orderNumber' => $order['OWebOrderReFund']['orderNumber'],
+            'orderData'   => $this->api->paramToXml($order),
+        ];
+        return $this->api->request('refundOrder', $params);
+    }
 }
