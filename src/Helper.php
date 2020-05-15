4cc4e64426a1c847b90c6a98ac8edbd28e3d1e06
qiliu <qiliuf@gmail.com>
2020-05-15 16:45:09 +0800

    update

--- src/Helper.php
+++ src/Helper.php
@@ -5,16 +5,4 @@ namespace Wored\ZiMaoDaSdk;
 
 class Helper
 {
-    static public function dump($param)
-    {
-        echo '<pre>';
-        var_dump($param);
-        echo '</pre>';
-    }
-
-    static public function dd($param)
-    {
-        self::dump($param);
-        exit;
-    }
 }
