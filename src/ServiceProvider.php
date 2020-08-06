b788addc03e0b0380920a8c7959497332c4ad0e3
qiliu <qiliuf@gmail.com>
2020-08-06 10:04:14 +0800

    删除无用数据

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,12 +4,6 @@ namespace Wored\ZiMaoDaSdk;
 
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
-use Slpcode\WangDianTongSdk\Basic\Basic;
-use Slpcode\WangDianTongSdk\Goods\Goods;
-use Slpcode\WangDianTongSdk\Purchase\Purchase;
-use Slpcode\WangDianTongSdk\Refund\Refund;
-use Slpcode\WangDianTongSdk\Stock\Stock;
-use Slpcode\WangDianTongSdk\Trade\Trade;
 
 class ServiceProvider implements ServiceProviderInterface
 {
